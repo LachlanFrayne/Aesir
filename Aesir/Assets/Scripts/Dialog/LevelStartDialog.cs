fd53373c049e102f873b4b84ba954632c6bdda9c
JoshMosk <36465182+JoshMosk@users.noreply.github.com>
2018-11-14 17:45:04 +1100

    Added in pre fight dialog system

---
+++ Aesir/Assets/Scripts/Dialog/LevelStartDialog.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelStartDialog : MonoBehaviour
+{
+
+	[TextArea(3,10)]
+	public string dialogText;
+	public AudioClip dialogAudio;
+
+	public AudioSource source;
+	public Text dialogBox;
+
+	private void Start()
+	{
+		source = GameObject.Find("Camera").GetComponent<AudioSource>();
+		dialogBox = GameObject.Find("DialogTextBox").GetComponent<Text>();
+	}
+
+	public void StartDialog()
+	{
+		StopAllCoroutines();
+		StartCoroutine(TypeDialog());
+	}
+
+	IEnumerator TypeDialog()
+	{
+		dialogBox.text = "";
+
+		foreach (char letter in dialogText.ToCharArray())
+		{
+			dialogBox.text += letter;
+
+			yield return null;
+		}
+
+		
+	}
+}
---
+++ Aesir/Assets/Scripts/Turn system/LevelStartTurn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStartTurn : StateMachineBehaviour
+{
+	public LevelStartDialog levelStartDialog;
+
+	private float timer;
+
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		levelStartDialog = animator.GetComponent<LevelStartDialog>();
+
+		levelStartDialog.source.PlayOneShot(levelStartDialog.dialogAudio, 1f);
+
+		levelStartDialog.StartDialog();
+
+		timer = levelStartDialog.dialogAudio.length;
+	}
+
+	public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+		{
+			animator.SetBool("PlayerTurn", true);
+		}
+	}
+
+	
+}
