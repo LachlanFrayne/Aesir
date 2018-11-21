03f8a253f003270595cc6db8478291defdab06d6
LachlanFrayne <39174585+LachlanFrayne@users.noreply.github.com>
2018-11-21 15:32:11 +1100

    Beta build

---
+++ Aesir/Assets/Scripts/Audio/MusicDelay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicDelay : MonoBehaviour {
+
+    public AudioSource music;
+    float timer;
+	void Start () {
+
+        music.enabled = false;
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 15)
+        {
+            music.enabled = true;
+        }
+    }
+
+}
